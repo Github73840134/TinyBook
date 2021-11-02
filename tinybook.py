6f9270133b9133f82cdb1dadb10a24d87a901d2b
Seth Edwards <35036025+Github73840134@users.noreply.github.com>
2021-11-02 16:15:57 +0000

    -

--- tinybook.py
+++ tinybook.py
@@ -9,9 +9,9 @@ __version__ = '1.0.3'
 __info__ = '©2021 Seth Edwards.'
 __metadata_ver__ = '1.0'
 __port__ = 'CPython'
-__releasenotes__ = '''-Added self updating\n
--Added release notes section\n
--Added release notes variable to the library, see the same notes via tinybook.__releasenotes__'''
+__releasenotes__ = '''- Added self updating (Only works on python 3.5.9 and above)\n
+- Added release notes section\n
+- Added release notes variable to the library, see the same notes via tinybook.__releasenotes__'''
 __license__ = '''
 ©2021 Seth Edwards
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
