25acff6d727cfad6cb09a979164f416256a3f3f6
Seth Edwards <35036025+Github73840134@users.noreply.github.com>
2021-11-05 21:40:17 +0000

    n

--- generateudf.py
+++ generateudf.py
@@ -6,13 +6,22 @@ def walk(ls,lst={}):
 	for i in ls:
 		if os.path.isdir(i):
 			lst[i] = {'action':'+d'}
+			print("Entering Directory:",i)
 			os.chdir(i)
 			lst.update(walk(os.listdir()))
+			print("Exiting Directory:",i)
 			os.chdir('../')
 		else:
-			lst[i] = {'action':'+','data':open(i,'r').read()}
+			print("Adding File:",i)
+			try:
+				lst[i] = {'action':'+','data':open(i,'r').read()}
+				print("File Added:",i)
+			except:
+				print("Adding File",i,"Failed")
 	return lst
-e = walk(os.listdir())
-print(e)
+print("Starting")
+e = {'data':walk(os.listdir())}
 os.chdir('../')
+print("Generating")
 json.dump(e,open('examples.udf','w+'))
+print("Done")
